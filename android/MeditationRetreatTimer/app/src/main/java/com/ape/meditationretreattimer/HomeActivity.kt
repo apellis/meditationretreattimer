e3a9d52cea9b583b5c5bf8f905506713e9cee340
Alex Ellis <apellis@gmail.com>
2023-02-22 15:50:36 -0800

    Allow new Timers to have user defined names given in an AlertDialog

--- android/MeditationRetreatTimer/app/src/main/java/com/ape/meditationretreattimer/HomeActivity.kt
+++ android/MeditationRetreatTimer/app/src/main/java/com/ape/meditationretreattimer/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.ape.meditationretreattimer
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ape.meditationretreattimer.data.AppDatabase
 import com.ape.meditationretreattimer.data.TimerDao
@@ -31,10 +33,17 @@ class HomeActivity : AppCompatActivity(), OnItemClickListener {
         binding.timersList.adapter = TimerListItemAdapter(this, timers, this)
 
         binding.addTimer.setOnClickListener {
-            // TODO: set id to a fresh id
-            // TODO: popup dialog to set a name
-            timerDao.insert(Timer("foo", ""))
-            refreshTimers()
+            val input = EditText(this)
+            val builder = AlertDialog.Builder(this)
+                .setTitle("New timer")
+                .setMessage("Give the new timer a name")
+                .setPositiveButton("OK") { _, _ ->
+                    timerDao.insert(Timer(input.text.toString(), ""))
+                    refreshTimers()
+                }
+                .setCancelable(true)
+                .setView(input)
+                .show()
         }
     }
 
